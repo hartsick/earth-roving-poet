961333b01486e515aab9e88d1f6543e8a5e0c976
Christa Hartsock <christa.hartsock@gmail.com>
2015-05-16 14:41:36 -0700

    tweeting descriptions of most recent earthrover update

--- earth_rover.py
+++ earth_rover.py
@@ -3,9 +3,11 @@ from twython import Twython
 import config
 
 class EarthRover(object):
+    user_id = 2714673572
+    username = "EarthRoverBot"
+
     def __init__(self, twitter):
         self.twitter = twitter
-        self.user_id = 2714673572
 
     def get_recent_statuses(self, number=100):
         '''
@@ -36,9 +38,27 @@ class EarthRover(object):
 
         return tweets
 
+    @classmethod
+    def get_media_url(cls, status):
+        if status['entities']['media']:
+            url = status['entities']['media'][0]['media_url']
+            if config.DEBUG:
+                print url
+            return url
+
+    @classmethod
+    def get_status_url(cls, status):
+        url = "http://twitter.com/{0}/status/{1}".format(cls.username, status['id_str'])
+        if config.DEBUG:
+            print url
+        return url
+
+
 
 if __name__ == '__main__':
 
-    # EarthRover(twitter).get_recent_statuses()
     twitter = Twython(*config.twitter_cred)
-    print(EarthRover(twitter).get_recent_statuses(5))
+    bot = EarthRover(twitter)
+    status = bot.get_recent_statuses(5)[-1]
+    bot.get_media_url(status)
+    bot.get_status_url(status)
--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from time import sleep
 from twython import Twython
 import config
 from image_to_text import ImageToText
-import earth_rover as EarthRover
+from earth_rover import EarthRover
 
 def do_the_thing():
     twitter = Twython(*config.twitter_cred)
-    text = "hello my future girlfriend"
 
-    # rover_status = EarthRover.get_most_recent_status(twitter)
-    # response = ImageToText(rover_status.image_url).response_for_image()
+    rover_status = EarthRover(twitter).get_recent_statuses()[-1]
+    img_url = EarthRover.get_media_url(rover_status)
 
+    text = ImageToText(img_url).top_caption()
+    text += " " + EarthRover.get_status_url(rover_status)
 
     if config.DEBUG:
         print "LOCAL: Updated status with: '{0}'".format(text)
